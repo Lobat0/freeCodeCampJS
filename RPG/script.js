568cca259af8e3308decf752a7195e1de5d778f4
lobato <lobato.filipe97@gmail.com>
2023-12-22 15:12:03 -0300

    Update script.js

--- RPG/script.js
+++ RPG/script.js
@@ -27,6 +27,12 @@ const locations = [
         "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
         "button functions": [buyHealth, buyWeapon, goTown],
         text: "You enter the store."
+    },
+    {
+        name: "cave",
+        "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+        "button functions": [fightSlime, fightBeast, goTown],
+        text: "You enter the cave. You see some monsters."
     }
 ];
 
@@ -45,7 +51,7 @@ function goStore() {
 }
 
 function goCave() {
-    console.log("Going to cave.")
+    update(locations[2]);
 }
 
 function fightDragon() {
@@ -67,7 +73,7 @@ function update(location) {
     button1.onclick = location["button functions"][0];
     button2.onclick = location["button functions"][1];
     button3.onclick = location["button functions"][2];
-    text.innerText = "You are in the town square. You see a sign that says \"Store\"."
+    text.innerText = location.text;
 
     button1.innerText = "Buy 10 health (10 gold)";
     button2.innerText = "Buy weapon (30 gold)";
@@ -76,4 +82,12 @@ function update(location) {
     button2.onclick = buyWeapon;
     button3.onclick = goTown;
     text.innerText = "You enter the store."
+}
+
+function fightSlime() {
+
+}
+
+function fightBeast() {
+
 }
